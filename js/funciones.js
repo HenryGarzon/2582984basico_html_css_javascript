c29fc301105556ca3f6c2662721b64b2a1745882
Luis.O <129633596+olwpad@users.noreply.github.com>
2023-04-28 08:46:26 -0500

    menu

--- js/funciones.js
+++ js/funciones.js
@@ -17,8 +17,25 @@
                         break;
                     case 4:
                         document.getElementById("muestra").innerHTML = "<iframe src='eventos.html'></iframe>";
-
                         break;
+
+                    case 4:
+                        document.getElementById("muestra").innerHTML = '<img src="https://catedras.facet.unt.edu.ar/prog1/wp-content/uploads/sites/10/2014/08/progra.png"/>';
+                        break;
+                    case 5:
+                        document.getElementById("muestra").innerHTML = '<img src="https://slideplayer.es/16475745/96/images/slide_1.jpg"/>';
+                        break;
+                    case 6:
+                        document.getElementById("muestra").innerHTML = '<img src="https://www.ardiseny.es/wordpress/http://wp.ardiseny.es/wp-content/uploads/2015/03/phpCode.png"/>';
+                        break;
+                    case 7:
+                        document.getElementById("muestra").innerHTML = '<img src="https://1.bp.blogspot.com/-MQtfb5bYGRY/U2HLr2TYAMI/AAAAAAAAAgY/RrzIpdbAGrk/s1600/Paradigmas+de+Programaci%C3%B3n+IV+-+Programaci%C3%B3n+Orientada+a+Objetos.jpg"/>';
+                        break;
+                    case 8:
+                        document.getElementById("muestra").innerHTML = "<iframe src='lUIS.html'></iframe>";
+                        break;
+
+
                 }
 
             }
